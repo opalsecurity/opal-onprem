06b44777a9db43af3159ee48f0673f5bee8756e1
Andri Bjarnason <andri@opal.dev>
2025-07-25 14:54:34 -0400

    Fix required_providers in opal-onprem terraform file

--- aws/main.tf
+++ aws/main.tf
@@ -1,8 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.67.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11.0"
     }
   }
 }
